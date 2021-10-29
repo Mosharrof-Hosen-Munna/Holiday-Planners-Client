8743dbcb8665deab07b55c9ead56e6fe35c07179
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2021-10-29 20:14:43 +0600

    create destination card

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Cover from "./Cover/Cover";
+import TravelDestination from "./TravelDestination/TravelDestination";
 
 const Home = () => {
   return (
     <>
       <Cover></Cover>
+      <TravelDestination></TravelDestination>
     </>
   );
 };
--- src/Components/Home/TravelDestination/TravelDestination.js
+++ src/Components/Home/TravelDestination/TravelDestination.js
@@ -1,33 +1,22 @@
 import React from "react";
 import { Card, Container, Row } from "react-bootstrap";
 import travelImage from "../../../images/cover.jpg";
+import TravelDestinationCard from "../TravelDestinationCard/TravelDestinationCard";
 
 const TravelDestination = () => {
   return (
-    <section className="py-5">
+    <section className="py-5 bg-light">
       <Container>
+        <div className="text-center">
+          <h1>Amazing Tours</h1>
+          <p className="text-secondary">
+            Trending, Best Selling Tours And Fun Destinations
+          </p>
+        </div>
         <Row lg={3} md={2} xs={1}>
-          <Row>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={travelImage} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>Cras justo odio</ListGroupItem>
-                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                <ListGroupItem>Vestibulum at eros</ListGroupItem>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Body>
-            </Card>
-          </Row>
+          <TravelDestinationCard></TravelDestinationCard>
+          <TravelDestinationCard></TravelDestinationCard>
+          <TravelDestinationCard></TravelDestinationCard>
         </Row>
       </Container>
     </section>
